a9c2e0e03dd75550b86a0ff4e6586de2b9334922
hunter.l <751754949@qq.com>
2019-08-01 10:48:37 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -4,38 +4,41 @@ import Index from '@/views/Index.vue'
 Vue.use(Router)
 
 const IndexRoute = {
-  path: '/',
-  component: Index,
-  redirect: '/login',
-  children: []
+    path: '/',
+    component: Index,
+    redirect: '/login',
+    children: []
 }
 
 let routes = [
-  IndexRoute,
-  {
-    path: '/login',
-    component: () => import(/* webpackChunkName: "login" */ '@/views/login/index.vue')
-  },
-  {
-    path: '*',
-    redirect: '/login'
-  }
+    IndexRoute,
+    {
+        path: '/login',
+        component: () =>
+            import ( /* webpackChunkName: "login" */ '@/views/login/index.vue')
+    },
+    {
+        path: '*',
+        redirect: '/login'
+    }
 ]
 
 const routerContext = require.context('./', true, /index\.js$/)
 routerContext.keys().forEach(route => {
-  // 如果是根目录的 index.js 、不处理
-  if (route.startsWith('./index')) {
-    return
-  }
-  const routerModule = routerContext(route)
-  /**
-  * 兼容 import export 和 require module.export 两种规范
-  */
-  IndexRoute.children = [...IndexRoute.children, ...(routerModule.default || routerModule)]
-})
-
+        // 如果是根目录的 index.js 、不处理
+        if (route.startsWith('./index')) {
+            return
+        }
+        const routerModule = routerContext(route)
+            /**
+             * 兼容 import export 和 require module.export 两种规范
+             */
+        IndexRoute.children = [...IndexRoute.children, ...(routerModule.default || routerModule)]
+    })
+    /**
+     * 在此作权限菜单过滤
+     */
 export default new Router({
-  mode: 'hash',
-  routes: routes
+    mode: 'hash',
+    routes: routes
 })
